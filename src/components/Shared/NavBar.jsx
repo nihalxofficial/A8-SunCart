de617e3131933eb69fb1fbb2ec3a15a78c4832e8
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-05-03 13:01:45 +0600

    Conditional session added

--- src/components/Shared/NavBar.jsx
+++ src/components/Shared/NavBar.jsx
@@ -4,10 +4,17 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { Menu, Sun, X } from 'lucide-react';
 import { FaHome, FaShoppingBasket, FaUser } from 'react-icons/fa';
-import { Button } from '@heroui/react';
+import { Button, Avatar } from '@heroui/react';
 import { useRouter } from 'next/navigation';
+import { authClient } from '@/lib/auth-client';
+import { MdOutlineLogout } from 'react-icons/md';
 
 const Navbar = () => {
+
+    const { data: session, isPending, error } = authClient.useSession()
+    const user = session?.user;
+    console.log(user);
+
     const router = useRouter();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const links = <>
@@ -56,20 +63,51 @@ const Navbar = () => {
                     <div className="hidden md:flex items-center space-x-8">
                         {links}
                     </div>
+                    {user ?
+                        <div className='hidden md:flex items-center gap-4'>
+                            <div className="relative group">
+                                <div className="absolute -inset-0.5 bg-linear-to-r from-sky-500 to-amber-500 rounded-full opacity-75 group-hover:opacity-100 transition duration-300 blur"></div>
+                                <Avatar className="relative">
+                                    <Avatar.Image
+                                        alt={user?.name || "User"}
+                                        src={user?.image}
+                                        className="object-cover"
+                                    />
+                                    <Avatar.Fallback className="bg-linear-to-r from-sky-500 to-cyan-400 text-white">
+                                        {user?.name?.charAt(0)}
+                                    </Avatar.Fallback>
+                                </Avatar>
+                            </div>
 
-                    <div className="hidden md:flex items-center gap-3">
-                        <Button onClick={()=>router.push("/signin")}
-                            className="bg-transparent border-2 border-sky-500 text-sky-600 hover:bg-sky-50 hover:border-sky-600 font-semibold px-6 py-2 rounded-lg transition duration-200"
-                        >
-                            Sign In
-                        </Button>
-                        <Button onClick={()=>router.push("/signup")}
-                            variant='secondary'
-                            className="bg-linear-to-r from-amber-500 to-amber-600 hover:from-amber-600 hover:to-amber-700 text-white font-semibold px-6 py-2 rounded-lg shadow-md hover:shadow-lg transition duration-200"
-                        >
-                            Sign Up
-                        </Button>
-                    </div>
+                            <div className="flex flex-col">
+                                <span className="text-xs text-stone-400">Welcome back,</span>
+                                <h2 className="text-sm font-semibold text-stone-700 line-clamp-1 max-w-30">
+                                    {user?.name}
+                                </h2>
+                            </div>
+
+                            <Button
+                                className="bg-linear-to-r from-rose-500 to-rose-500 hover:from-rose-600 hover:to-pink-600 text-white flex items-center gap-2 px-4 py-2 rounded-lg transition-all duration-200 shadow-md hover:shadow-lg"
+                            >
+                                <MdOutlineLogout size={16} />
+                                <span className="text-sm font-medium">Sign Out</span>
+                            </Button>
+                        </div> :
+                        <div className="hidden md:flex items-center gap-3">
+                            <Button onClick={() => router.push("/signin")}
+                                className="bg-transparent border-2 border-sky-500 text-sky-600 hover:bg-sky-50 hover:border-sky-600 font-semibold px-6 py-2 rounded-lg transition duration-200"
+                            >
+                                Sign In
+                            </Button>
+                            <Button onClick={() => router.push("/signup")}
+                                variant='secondary'
+                                className="bg-linear-to-r from-amber-500 to-amber-600 hover:from-amber-600 hover:to-amber-700 text-white font-semibold px-6 py-2 rounded-lg shadow-md hover:shadow-lg transition duration-200"
+                            >
+                                Sign Up
+                            </Button>
+                        </div>}
+
+
 
                     <div className="md:hidden">
                         <button
@@ -88,12 +126,12 @@ const Navbar = () => {
                         <div className="flex flex-col space-y-4 justify-start">
                             {links}
                             <div className="flex flex-col gap-3 pt-2">
-                                <Button onClick={()=>router.push("/signin")}
+                                <Button onClick={() => router.push("/signin")}
                                     className="bg-transparent border-2 border-sky-500 text-sky-600 hover:bg-sky-50 font-semibold py-2 rounded-lg transition duration-200 w-full"
                                 >
                                     Sign In
                                 </Button>
-                                <Button onClick={()=>router.push("/signup")}
+                                <Button onClick={() => router.push("/signup")}
                                     variant='secondary'
                                     className="bg-linear-to-r from-amber-500 to-amber-600 hover:from-amber-600 hover:to-amber-700 text-white font-semibold py-2 rounded-lg shadow-md transition duration-200 w-full"
                                 >
